83064ea076910de0cae46bb5039b5705a401c9e4
Ahmed <ahmed91alhaj@gmail.com>
2023-07-24 04:08:39 -0400

    Transform data with Selectors & Retrieve the days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from 'components/Appointment';
-import axios from "axios";
+// import { getAppointmentsForDay } from "helpers/selectors";
 
 const appointments = {
   "1": {
@@ -44,13 +45,19 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState(null);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
   useEffect(() => {
-    axios.get(`/api/days`).then(response => {
-      setDays([response.data])
-      console.log(response.data);
-    });
+    axios.get(`/api/days`)
+      .then(response => {
+        setDays(response.data)
+        console.log(response.data);
+      });
   }, []);
   return (
     <main className="layout">
@@ -63,8 +70,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            day={state.day}
             onChange={setDay} />
         </nav>
         <img
